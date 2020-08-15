3a3edf24574eff7c24a193aeb6126d3be2f05660
Adithyan <adithyan92014@gmail.com>
2020-08-15 23:45:33 +0530

    fetched & displayed satatlite data

--- src/components/Starlink.js
+++ src/components/Starlink.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import fetch from "./fetch";
 
 const Starlink = () => {
+  const { status, data } = useQuery("satalites", () =>
+    fetch("https://api.spacexdata.com/v4/starlink")
+  );
+  if (status === "loading") return <p>Loading...</p>;
+  if (status === "error") return <p>Error :(</p>;
+  console.info(data);
   return (
     <div>
-      Starlink
+      <h2>Satalites</h2>
+      {data.data.map((sat) => {
+        return (
+          <Link key={sat.id} to={`/characters/${sat.id}`}>
+            {sat.id} statalite
+            <br></br>
+          </Link>
+        );
+      })}
       <Link to="/">go Home </Link>
     </div>
   );
