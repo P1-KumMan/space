b780fe9f00876d08a82523c3e344891b2be8f1d3
Adithyan <adithyan92014@gmail.com>
2020-08-16 21:27:38 +0530

    fetched rockets and dragons

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { useQuery } from "react-query";
+import fetch from "./fetch";
+import { Link } from "react-router-dom";
 
 const Dragons = () => {
-  return <div>Dragons</div>;
+  const { status, data } = useQuery("rocks", () =>
+    fetch("https://api.spacexdata.com/v4/dragons")
+  );
+  if (status === "loading") return <p>Loading...</p>;
+  if (status === "error") return <p>Error :(</p>;
+  console.info(data);
+  return (
+    <div>
+      <h2>Dragons</h2>
+      {data.data.map((dragon) => {
+        console.log(dragon);
+        return (
+          <Link key={dragon.id} to={`/dragon/${dragon.id}`}>
+            {dragon.id} Dragon
+            <br></br>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Dragons;
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { useQuery } from "react-query";
+import fetch from "./fetch";
+import { Link } from "react-router-dom";
 
 const Rockets = () => {
-  return <div>Rockets</div>;
+  const { status, data } = useQuery("rocks", () =>
+    fetch("https://api.spacexdata.com/v4/rockets")
+  );
+  if (status === "loading") return <p>Loading...</p>;
+  if (status === "error") return <p>Error :(</p>;
+  console.info(data);
+  return (
+    <div>
+      <h2>Rockets</h2>
+      {data.data.map((rok) => {
+        console.log(rok);
+        return (
+          <Link key={rok.id} to={`/rocket/${rok.id}`}>
+            {rok.id} rocket
+            <br></br>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Rockets;
