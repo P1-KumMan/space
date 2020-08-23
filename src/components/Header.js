07e114c5bd07c1695e3f5c0ba080e58f58aa6683
Adithyan <adithyan92014@gmail.com>
2020-08-23 11:53:58 +0530

    home works!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link as Rlink } from "react-router-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Starlink from "./Starlink";
@@ -8,15 +8,74 @@ import Rockets from "./Rockets";
 import Rocket from "./Rocket";
 import Dragon from "./Dragon";
 import Dragons from "./Dragons";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { Link, Button } from "@material-ui/core";
+
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  menu: {
+    margin: "0 auto",
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "#CCC",
+    fontFamily: "sans-serif",
+    "& button": {
+      margin: theme.spacing(2),
+    },
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <BrowserRouter>
-        <Link to="/">Home</Link>
-        <Link to="/dragons">Dragon</Link>
-        <Link to="/Rockets">Rockets</Link>
-        <Link to="/Starlink">StarLink</Link>
+        <AppBar position="sticky">
+          <Toolbar>
+            {/* <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton> */}
+            <Typography className={classes.menuButton}>
+              <Link color="secondary" component={Rlink} to="/">
+                <Button color="">Home</Button>
+              </Link>
+            </Typography>
+            <Typography className={classes.menuButton}>
+              <Link color="secondary" component={Rlink} to="/dragons">
+                <Button color="">Dragon</Button>
+              </Link>
+            </Typography>
+            <Typography className={classes.menuButton}>
+              <Link color="secondary" component={Rlink} to="/rockets">
+                <Button color="">Rockets</Button>
+              </Link>
+            </Typography>
+            <Typography className={classes.menuButton}>
+              <Link color="secondary" component={Rlink} to="/starlink">
+                <Button color="">Starlink</Button>
+              </Link>
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/starlink" component={Starlink} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,177 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Grid, Paper, ButtonBase, Card } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        margin: "0",
+        backgroundColor: "black",
+    },
+    image: {
+        // width: 800,
+        // height: 500,
+    },
+    img: {
+        margin: "100px",
+        display: "block",
+        maxWidth: "100%",
+        maxHeight: "100%",
+    },
+    font: {
+        fontFamily: "sans-serif",
+        color: "white",
+    },
+    ca: {
+        backgroundColor: "black",
+    },
+}));
 export default function Home() {
-  return <div>Home</div>;
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <Paper className={classes.paper}>
+                <Grid container spacing={2} direction-xs-column>
+                    <Grid item xs={6} sm container direction="column">
+                        <Card className={classes.ca}>
+                            <Grid item xs>
+                                <Typography
+                                    gutterBottom
+                                    className={classes.font}
+                                    variant="subtitle1"
+                                >
+                                    First Orbital Class Rocket capable of
+                                    reflight
+                                </Typography>
+                                <Typography
+                                    variant="h2"
+                                    gutterBottom
+                                    className={classes.font}
+                                >
+                                    Falcon 9
+                                </Typography>
+                                <Typography
+                                    className={classes.font}
+                                    variant="body2"
+                                    color="textSecondary"
+                                >
+                                    Two-stage rocket designed and manufactured
+                                    by SpaceX for the reliable and safe
+                                    transport of satellites and the Dragon
+                                    spacecraft into orbit
+                                </Typography>
+                            </Grid>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <ButtonBase className={classes.image}>
+                            <img
+                                className={classes.img}
+                                alt="complex"
+                                src="https://www.spacex.com/static/images/backgrounds/f9_feature.jpg"
+                            />
+                        </ButtonBase>
+                    </Grid>
+                </Grid>
+            </Paper>
+            <Paper className={classes.paper}>
+                <Grid container spacing={2} direction-xs-column>
+                    <Grid item xs={6}>
+                        <ButtonBase className={classes.image}>
+                            <img
+                                className={classes.img}
+                                alt="complex"
+                                src="https://www.spacex.com/static/images/dragon/DR_2.jpg"
+                            />
+                        </ButtonBase>
+                    </Grid>
+                    <Grid item xs={6} sm container direction="column">
+                        <Grid item xs container direction="column" spacing={3}>
+                            <Grid item xs>
+                                <Typography
+                                    gutterBottom
+                                    className={classes.font}
+                                    variant="subtitle1"
+                                >
+                                    Sending humans and cargo into space
+                                </Typography>
+                                <Typography
+                                    variant="h2"
+                                    gutterBottom
+                                    className={classes.font}
+                                >
+                                    Dragon
+                                </Typography>
+                                <Typography
+                                    className={classes.font}
+                                    variant="body2"
+                                    color="textSecondary"
+                                >
+                                    The Dragon spacecraft is capable of carrying
+                                    up to 7 passengers to and from Earth orbit,
+                                    and beyond. It is the only spacecraft
+                                    currently flying that is capable of
+                                    returning significant amounts of cargo to
+                                    Earth, and is the first private spacecraft
+                                    to take humans to the space station.
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Paper>
+            <Paper className={classes.paper}>
+                <Grid container spacing={2} direction-xs-column>
+                    <Grid item xs={6} sm container direction="column">
+                        <Grid item xs container direction="row" spacing={3}>
+                            <Grid item xs>
+                                <Typography
+                                    gutterBottom
+                                    className={classes.font}
+                                    variant="subtitle1"
+                                >
+                                    Sending humans and cargo into space
+                                </Typography>
+                                <Typography
+                                    variant="h2"
+                                    gutterBottom
+                                    className={classes.font}
+                                >
+                                    Dragon
+                                </Typography>
+                                <Typography
+                                    className={classes.font}
+                                    variant="body2"
+                                    color="textSecondary"
+                                >
+                                    The Dragon spacecraft is capable of carrying
+                                    up to 7 passengers to and from Earth orbit,
+                                    and beyond. It is the only spacecraft
+                                    currently flying that is capable of
+                                    returning significant amounts of cargo to
+                                    Earth, and is the first private spacecraft
+                                    to take humans to the space station.
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <ButtonBase className={classes.image}>
+                            <img
+                                className={classes.img}
+                                alt="complex"
+                                src="https://www.spacex.com/static/images/falcon-heavy/FH_3.jpg"
+                            />
+                        </ButtonBase>
+                    </Grid>
+                </Grid>
+            </Paper>
+        </div>
+    );
 }
