b7b986d2498d418d7e0e3778c1d5b981dc3bd6a4
Adithyan <adithyan92014@gmail.com>
2020-08-29 15:25:28 +0530

    fixed header margin issue and added hide animation when scolled!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import "./App.css";
+import { Box } from "@material-ui/core";
 
 const App = () => {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+    return (
+        <Box style={{ backgroundColor: "white", margin: 0, padding: 0 }}>
+            <Header style={{ margin: 0 }} />
+        </Box>
+    );
 };
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,81 +13,105 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { Link, Button } from "@material-ui/core";
-
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Slide from "@material-ui/core/Slide";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  menu: {
-    margin: "0 auto",
-    display: "flex",
-    justifyContent: "center",
-    backgroundColor: "#CCC",
-    fontFamily: "sans-serif",
-    "& button": {
-      margin: theme.spacing(2),
+    root: {
+        flexGrow: 1,
     },
-  },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        color: "i",
+    },
+    title: {
+        flexGrow: 1,
+    },
+    app: {
+        margin: 0,
+        color: "white",
+        backgroundColor: "black",
+    },
 }));
+function HideOnScroll(props) {
+    const { children, window } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+}
 
 const Header = () => {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <BrowserRouter>
-        <AppBar position="sticky">
-          <Toolbar>
-            {/* <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton> */}
-            <Typography className={classes.menuButton}>
-              <Link color="secondary" component={Rlink} to="/">
-                <Button color="">Home</Button>
-              </Link>
-            </Typography>
-            <Typography className={classes.menuButton}>
-              <Link color="secondary" component={Rlink} to="/dragons">
-                <Button color="">Dragon</Button>
-              </Link>
-            </Typography>
-            <Typography className={classes.menuButton}>
-              <Link color="secondary" component={Rlink} to="/rockets">
-                <Button color="">Rockets</Button>
-              </Link>
-            </Typography>
-            <Typography className={classes.menuButton}>
-              <Link color="secondary" component={Rlink} to="/starlink">
-                <Button color="">Starlink</Button>
-              </Link>
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/starlink" component={Starlink} />
-          <Route path="/sat/:id" component={Sat} />
-          <Route path="/rockets" component={Rockets} />
-          <Route path="/rocket/:id" component={Rocket} />
-          <Route path="/dragons" component={Dragons} />
-          <Route path="/dragon/:id" component={Dragon} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <BrowserRouter>
+                <HideOnScroll>
+                    <AppBar className={classes.app}>
+                        <Toolbar>
+                            <IconButton
+                                edge="start"
+                                className={classes.menuButton}
+                                color="inherit"
+                                aria-label="menu"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography className={classes.menuButton}>
+                                <Link color="inherit" component={Rlink} to="/">
+                                    <Button color="inherit">Home</Button>
+                                </Link>
+                            </Typography>
+                            <Typography className={classes.menuButton}>
+                                <Link
+                                    color="inherit"
+                                    component={Rlink}
+                                    to="/dragons"
+                                >
+                                    <Button color="inherit">Dragon</Button>
+                                </Link>
+                            </Typography>
+                            <Typography className={classes.menuButton}>
+                                <Link
+                                    color="inherit"
+                                    component={Rlink}
+                                    to="/rockets"
+                                >
+                                    <Button color="inherit">Rockets</Button>
+                                </Link>
+                            </Typography>
+                            <Typography className={classes.menuButton}>
+                                <Link
+                                    color="inherit"
+                                    component={Rlink}
+                                    to="/starlink"
+                                >
+                                    <Button color="inherit">Starlink</Button>
+                                </Link>
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                </HideOnScroll>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/starlink" component={Starlink} />
+                    <Route path="/sat/:id" component={Sat} />
+                    <Route path="/rockets" component={Rockets} />
+                    <Route path="/rocket/:id" component={Rocket} />
+                    <Route path="/dragons" component={Dragons} />
+                    <Route path="/dragon/:id" component={Dragon} />
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default Header;
