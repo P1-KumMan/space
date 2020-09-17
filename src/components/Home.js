b3065141372919c7f4afdfcef89480fbf6f1f040
Adithyan <adithyan92014@gmail.com>
2020-09-17 13:23:09 +0530

    agin Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,6 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import {
-    Grid,
-    Paper,
-    ButtonBase,
-    Card,
-    CardContent,
-    CardHeader,
-} from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import LatestCard from "./ui/LatestCard.js";
 import F9Card from "./ui/F9Card";
 import FHCard from "./ui/FHCard";
--- src/components/ui/F9Card.js
+++ src/components/ui/F9Card.js
@@ -3,13 +3,10 @@ import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { useQuery } from "react-query";
 import fetch from "../fetch";
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 const useStyles = makeStyles({
