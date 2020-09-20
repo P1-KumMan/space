b97a78ec5971c83fe5d6f2c64bf98ddfbb41b030
Adithyan <adithyan92014@gmail.com>
2020-09-20 15:34:38 +0530

    small correction

--- src/components/Starlink.js
+++ src/components/Starlink.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useQuery } from "react-query";
 import { makeStyles } from "@material-ui/core/styles";
 import fetch from "./fetch";
-import { ThemeProvider, Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
 import "fontsource-oswald";
 import "fontsource-antic-slab";
